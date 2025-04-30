0893df45863b3dd90ca0748262ba00b69dc5e448
Elif Özcan <ozcann159@gmail.com>
2025-05-01 01:10:30 +0300

    İlk gönderim: pupilica projesi

---
+++ TasarimCalismasi/app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tasarimcalismasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //sınıfın tasarımını belirtir.
+        //R = res klasörü (kaynaklar klasörü)
+    }
+}
